920216ef8b30ab93bf8ec0586ed6428348c59dc3
vectronic <hello.vectronic@gmail.com>
2022-05-25 09:50:32 +0100

    chore: add gh-pages branch

--- tests/integration_test.rs
+++
@@ -1,9 +0,0 @@
-use flowscripter_template_wasm_rust_library;
-use wasm_bindgen_test::*;
-
-wasm_bindgen_test_configure!(run_in_browser);
-
-#[wasm_bindgen_test]
-fn world_works() {
-    assert_eq!(4, flowscripter_template_wasm_rust_library::add(2, 2));
-}
